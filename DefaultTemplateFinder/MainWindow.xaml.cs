048965fa6a22c73b529ee5ebdeeb404e6d1a0976
erythana <c.berger18@gmail.com>
2021-07-19 09:46:00 +0200

    Initial commit - Working

---
+++ DefaultTemplateFinder/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace DefaultTemplateFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public ObservableCollection<Type> WindowsControlsCollection { get; private set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            WindowsControlsCollection = new ObservableCollection<Type>(GetTypes());
+        }
+
+        private static IEnumerable<Type> GetTypes()
+        {
+            return Assembly.GetAssembly(typeof(Control))?.GetTypes()
+                .Where(t => t.IsPublic && !t.IsAbstract && typeof(Control).IsAssignableFrom(t)).OrderBy(t => t.Name);
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            textEditor.Text = "";
+            if (e.AddedItems[0] is not Type selectedType) return;
+
+            if (Application.Current.TryFindResource(selectedType) is { } defaultTemplate)
+            {
+                var memoryStream = new MemoryStream();
+                using var writer = new XmlTextWriter(memoryStream, Encoding.UTF8) 
+                    {Formatting = Formatting.Indented};
+                XamlWriter.Save(defaultTemplate, writer); ;
+                textEditor.Text = Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+            else
+            {
+                textEditor.Text =
+                    $"Could not find Resource for {selectedType.Name}, try it with its base class, {selectedType.BaseType}";
+            }
+                
+            
+                
+        }
+    }
+}
